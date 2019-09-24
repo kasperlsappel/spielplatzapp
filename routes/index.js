c71f65aadc7ac86c0789096e166a00c6568ded44
Christian Lahmer <christian.lahmer@gmail.com>
2019-09-24 12:31:53 +0200

    Datenbank hinzugefügt

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,33 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Spielplatz App' });
 });
 
+
+router.get('/db', function(req, res, next) {
+
+  const { Client } = require('pg');
+
+  console.log(`DB Server url: ${process.env.DATABASE_URL}`)
+  const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: false,
+  });
+  
+  client.connect();
+  
+  var rows = [];
+
+  client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, r) => {
+    if (err) throw err;
+    for (let row of r.rows) {
+      rows.push(row)
+    }
+    client.end();
+
+    res.render('db', { title: 'Spielplatz App DB Test', rows : rows });
+  });
+});
+
+
+
+
 module.exports = router;
